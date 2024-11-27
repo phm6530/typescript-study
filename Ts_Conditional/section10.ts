8cef888a936a40d8b7400fbba7c9e5aba3969958
phm6530 <squirrel309@naver.com>
2024-11-27 18:12:54 +0900

    맵드 타입 연습

---
+++ Ts_Conditional/section10.ts
@@ -0,0 +1,40 @@
+/**
+ * 조건부 타입
+ */
+type A = number extends string ? string : number;
+
+type ObjA = {
+  a: number;
+};
+
+type ObjB = {
+  a: number;
+  b: number;
+};
+
+type B = 1 extends number ? number : string;
+
+/**
+ * 조건부 타입
+ * string <-> number
+ */
+type TransformType<T> = T extends number ? string : number;
+let varA: TransformType<number>;
+let varB: TransformType<string>;
+
+/**
+ *
+ * @param text
+ * 오버로딩 시그니처
+ */
+
+function removeSpace<T extends string>(
+  text: T
+): T extends string ? string : never;
+function removeSpace(text: any) {
+  return text.trim();
+}
+
+let result = removeSpace("hi my names");
+
+export {};
